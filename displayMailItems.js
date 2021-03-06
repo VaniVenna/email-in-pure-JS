6acb15d5551641c2013fb2238993cbe8f9cf31f9
VaniVenna <73954300+VaniVenna@users.noreply.github.com>
2021-03-06 19:51:40 +0530

    Add files via upload

--- displayMailItems.js
+++ displayMailItems.js
@@ -1,35 +1,65 @@
-function displayMailItems(mailBoxList, mailContainer) {
+function displayMailItems(mailBoxList, mailTbody, contentContainer, commonCheck, deleteIconCommon) {
     this.mailBox = mailBoxList;
     this.init = function () {
         this.createMailBox();
     }
+    
     this.createMailBox = function () {
-        var mailBoxParent = document.createElement("section");
-        mailBoxParent.className = "mailBoxParent";
-
-        var mailBoxTable = document.createElement("table");
-        mailBoxParent.className = "mailBoxTable";
-
-        var mailBoxTbody = document.createElement("tbody");
-        mailBoxParent.className = "mailBoxTbody";
-        mailBoxTable.appendChild(mailBoxTbody);
-        mailBoxParent.appendChild(mailBoxTable);
-        mailContainer.appendChild(mailBoxParent);
+        
         for (const key in this.mailBox) {
+            this.check = document.createElement("input");
+            this.check.type = "checkbox";
+            this.check.className = "check";
+
+            var deleteIcon = document.createElement("button");
+            this.deleteIcon = deleteIcon;
+            this.deleteIcon.textContent = "delete";
+            this.deleteIcon.className = "delete";
+            this.deleteIcon.disabled = true;
+
             var actParent = document.createElement("tr");
             actParent.className = key;
-            mailBoxTbody.appendChild(actParent);
+            mailTbody.appendChild(actParent);
 
             for (const iterator in this.mailBox[key]) {
                 if (iterator != "content") {
+                    actParent.prepend(this.check);
                     var right = document.createElement("td");
                     right.className = iterator;
                     right.textContent = this.mailBox[key][iterator];
                     actParent.appendChild(right);
+                    actParent.appendChild(this.deleteIcon);
                 }
                 else {
                     actParent.setAttribute("data-content", this.mailBox[key][iterator]);
                 }
+            }
+
+            var clickInstance = new clickEvents(contentContainer, deleteIconCommon);
+
+            actParent.onclick = function (e) {
+                clickInstance.replaceData(e.currentTarget);
+            }
+
+            this.check.onclick = function (e) {
+                e.stopPropagation();
+                // console.log(self.clickInstance);
+                clickInstance.enableDeleteIcons(e);
+            }
+
+            this.deleteIcon.onclick = function (e) {
+                clickInstance.deleteNode(e);
+            }
+
+            commonCheck.onclick = function (e) {
+                clickInstance.selectAll(e, this.deleteIcon);
+            }.bind(this);
+
+            deleteIconCommon.onclick = function (e) {
+                clickInstance.deleteAll(e);
+                commonCheck.checked = false;
+                this.disabled = true;
+                // commonCheck.disabled = true;
             }
         }
     }
--- leftMenu.js
+++ leftMenu.js
@@ -13,8 +13,41 @@ function leftPanel(menuItems, mailContainer) {
             this.menuItems.appendChild(menuItem);
         }
         mailContainer.appendChild(this.menuItems);
+        
+        var mailBoxParent = document.createElement("section");
+        mailBoxParent.className = "mailBoxParent";
+        
+        var mailBoxTable = document.createElement("table");
+        mailBoxTable.className = "mailBoxTable";
+
+        this.mailBoxTbody = document.createElement("tbody");
+        this.mailBoxTbody.id = "mailBoxTbody";
+        
+        this.content = document.createElement("div");
+        this.content.className = "content";
+        
+        var commonDiv = document.createElement("div")
+        commonDiv.className = "control-container";
+
+        this.commonCheck = document.createElement("input");
+        this.commonCheck.type = "checkbox";
+        this.commonCheck.className = "checkCommon";
+
+        this.deleteIconCommon = document.createElement("button");
+        this.deleteIconCommon.textContent = "delete";
+        this.deleteIconCommon.className = "delete";
+        this.deleteIconCommon.disabled = true;
+
+        mailBoxParent.append(commonDiv);
+        commonDiv.append(this.commonCheck, this.deleteIconCommon);
+
+        document.body.appendChild(this.content);
+        mailBoxTable.appendChild(this.mailBoxTbody);
+        mailBoxParent.appendChild(mailBoxTable);
+        mailContainer.appendChild(mailBoxParent);
     }
     this.loadMailItems = function(targetMailBox, mailItems) {
+        this.content.innerHTML = "";
         switch(targetMailBox.id) {
             case "inboxId": this.displayMailContent(mailItems[0].inbox);
             break;
@@ -28,8 +61,11 @@ function leftPanel(menuItems, mailContainer) {
         }
     }
     this.displayMailContent = function(mailBoxList) {
-        new displayMailItems(mailBoxList, mailContainer);
-    } 
-    
+        var removeChild = document.getElementById("mailBoxTbody");
+        if (removeChild.childNodes.length > 0)
+            removeChild.innerHTML = "";
+
+        new displayMailItems(mailBoxList, this.mailBoxTbody, this.content, this.commonCheck, this.deleteIconCommon);
+    }
     this.init();
 }
